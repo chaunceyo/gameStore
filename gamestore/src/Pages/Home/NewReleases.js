9e1b0a97b3d8f7d4484c08db1d5f78a37fb72091
Chaunceyo <chaunceyogletree725@gmail.com>
2024-09-24 02:46:42 -0400

    wip

---
+++ gamestore/src/Pages/Home/NewReleases.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import './Home.css'
+
+const NewReleases = () => {
+    return(
+        <div>
+            <div className='new-section'>
+                <h1>New Releases</h1>
+            </div>
+
+            <div className='Home-container'>
+                <div className='new-items'>
+                <img 
+                src='https://image.api.playstation.com/vulcan/ap/rnd/202405/2216/cbb03393f0ab1149f2b89a8194ce19e596a24fa5bec6526a.png'
+                alt="Description "
+                width='300'
+                height='300'></img>
+                <p>Dragon Ball: Sparking! Zero</p>
+                </div>
+
+                <div className='new-items'>
+                <img 
+                src='https://image.api.playstation.com/vulcan/ap/rnd/202406/0521/47126dbd889a804f04e5b80ea35973622b041c060c9e1249.jpg'
+                alt="Description "
+                width='300'
+                height='300'></img>
+                <p>NBA 2K25</p>
+                </div>
+
+                <div className='new-items'>
+                <img 
+                src='https://support.activision.com/content/dam/atvi/support/product/hero/BO6-FOB.jpg'
+                alt="Description "
+                width='250'
+                height='300'></img>
+                <p>Call of Duty: Black Ops 6</p>
+                </div>
+
+                <div className='new-items'>
+                <img 
+                src='https://image.api.playstation.com/vulcan/ap/rnd/202302/2407/06ae9175e65c0965e44b12b4f34f7fe5aa491bbf7e5ce5c6.png'
+                alt="Description "
+                width='250'
+                height='300'></img>
+                <p>Naruto X Boruto Ninja Storm Connections</p>
+                </div>
+
+                <div className='new-items'>
+                <img 
+                src='https://image.api.playstation.com/vulcan/ap/rnd/202406/1011/8bf6908bc4a21488c7bfa3bbf75ceca8f4c5137bafc136ae.png'
+                alt="Description "
+                width='250'
+                height='300'></img>
+                <p>Madden 25</p>
+                </div>
+
+                <div className='new-items'>
+                <img 
+                src='https://image.api.playstation.com/vulcan/ap/rnd/202408/2112/6db314e57a2440ba6ff830a195c2d3f384d1175eebc93fd9.png'
+                alt="Description "
+                width='250'
+                height='300'></img>
+                <p>EA Sports: College Football 25</p>
+                </div>
+
+                <div className='new-items'>
+                <img 
+                src='https://image.api.playstation.com/vulcan/ap/rnd/202405/2117/bd406f42e9352fdb398efcf21a4ffe575b2306ac40089d21.png'
+                alt="Description "
+                width='250'
+                height='300'></img>
+                <p>Black Myth: Wukong</p>
+                </div>
+
+                <div className='new-items'>
+                <img 
+                src='https://image.api.playstation.com/vulcan/ap/rnd/202308/0312/aff71a0ced271048f5079b1fcf715bcb45110efc13e9704a.png'
+                alt="Description "
+                width='250'
+                height='300'></img>
+                <p>Tekken 8</p>
+                </div>
+
+                <div className='new-items'>
+                <img 
+                src='https://image.api.playstation.com/vulcan/ap/rnd/202207/1210/4xJ8XB3bi888QTLZYdl7Oi0s.png'
+                alt="Description "
+                width='300'
+                height='300'></img>
+                <p>God of War Ragnarök</p>
+                </div>
+
+                <div className='new-items'>
+                <img 
+                src='https://image.api.playstation.com/vulcan/ap/rnd/202305/1515/93797636f47b37b74e9d3740e17ae1306aaf539c7952a703.jpg'
+                alt="Description "
+                width='300'
+                height='300'></img>
+                <p>Mortal Kombat 1</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default NewReleases;
