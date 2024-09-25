6a53ca3c6ea49d2592781380a2d0f403f3a754ab
Chaunceyo <chaunceyogletree725@gmail.com>
2024-09-25 17:37:57 -0400

    added psBestSellers

--- gamestore/src/Pages/Playstation/PSBestSellers.js
+++ gamestore/src/Pages/Playstation/PSBestSellers.js
@@ -11,38 +11,38 @@ const PSBestSellers = () => {
             <div className='psConsole-container'>
                 <div className='psConsole-items'>
                     <img 
-                    src='https://i5.walmartimages.com/asr/257979b9-5e09-4d49-a9c3-1d0af2e3b368.0c9295d5e6d8aa8af7d454d52e9079b7.jpeg'
+                    src='https://m.media-amazon.com/images/I/81WUPcfQ9OL._AC_UF1000,1000_QL80_.jpg'
                     alt="Description "
                     width='300'
                     height='300'></img>
-                    <p>PlayStation 5  <br/> Console </p>
+                    <p>Marvel's Spiderman 2</p>
                 </div>
                 
                 <div className='psConsole-items'>
                     <img 
-                    src='https://media.gamestop.com/i/gamestop/11108141/Sony-PlayStation-5-Digital-Edition-Console'
+                    src='https://products.eneba.games/resized-products/oTmXEq8cssvbEvwL3_69UVLd5a-ajU-uKP0gRC3LEqI_350x200_1x-0.jpeg'
                     alt="Description "
                     width='300'
                     height='300'></img>
-                    <p>PlayStation 5  <br/> Digital Edition Console </p>
+                    <p>Ghost of Tsushima: Director's Cut </p>
                 </div>
 
                 <div className='psConsole-items'>
                     <img 
-                    src='https://media.gamestop.com/i/gamestop/20009347/Sony-PlayStation-5-Slim-Console-Digital-Edition'
+                    src='https://m.media-amazon.com/images/I/81h2WhI4dtL._AC_UF1000,1000_QL80_.jpg'
                     alt="Description "
                     width='300'
                     height='300'></img>
-                    <p>PlayStation 5  <br/> Digital Edition Console - Slim</p>
+                    <p>Elden Ring</p>
                 </div>
 
                 <div className='psConsole-items'>
                     <img 
-                    src='https://m.media-amazon.com/images/I/41ECK5cY-2L._AC_UF894,1000_QL80_.jpg'
+                    src='https://m.media-amazon.com/images/I/81J6y4tm-WL._AC_UF894,1000_QL80_.jpg'
                     alt="Description "
                     width='300'
                     height='300'></img>
-                    <p>PlayStation 5  <br/> Disc Edition Console - Slim</p>
+                    <p>Call of Duty: Black Ops 6</p>
                 </div>
             </div>
         </div>
