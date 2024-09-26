1ae0a06a0246758c9c7d97b480581e992de8a783
Chaunceyo <chaunceyogletree725@gmail.com>
2024-09-26 03:11:45 -0400

    wip

--- gamestore/src/Pages/Playstation/PSBestSellers.js
+++ gamestore/src/Pages/Playstation/PSBestSellers.js
@@ -10,11 +10,10 @@ const PSBestSellers = () => {
             </div>
 
             <div className='psConsole-container'>
-                <div className='psConsole-items'>
-                    <div className='psSpiderman' 
-                            onMouseEnter={() => setIsHovered(true)}
-                            onMouseLeave={() => setIsHovered(false)}
-                    >
+                <div className='psConsole-items'
+                     onMouseEnter={() => setIsHovered(true)}
+                     onMouseLeave={() => setIsHovered(false)}
+                >
                         <img 
                         src='https://m.media-amazon.com/images/I/81WUPcfQ9OL._AC_UF1000,1000_QL80_.jpg'
                         alt="Description "
@@ -27,40 +26,82 @@ const PSBestSellers = () => {
                     {isHovered && (
                         <div className='psGIF'>
                             <img 
-                        src='https://media.tenor.com/9ATcv6bNTawAAAAM/peter-venom.gif'
+                        src='https://i.makeagif.com/media/10-12-2023/dDrfS7.gif'
                         alt='Description '
                         width='300'
                         height='300'></img>
                         </div>
                     )}
-                    </div>
                 </div>
                 
-                <div className='psConsole-items'>
+                <div className='psConsole-items'
+                     onMouseEnter={() => setIsHovered(true)}
+                     onMouseLeave={() => setIsHovered(false)}
+                >
                     <img 
                     src='https://gamebroslb.com/cdn/shop/products/image_53fd8473-3b4a-49ae-9501-369a9acf9abc.webp?v=1659994132'
                     alt="Description "
                     width='300'
                     height='300'></img>
                     <p>Ghost of Tsushima: Director's Cut </p>
+                    <p>$29.99</p>
+
+                    {isHovered && (
+                        <div className='psGIF'>
+                            <img 
+                        src='https://media.tenor.com/TCBvPlkAwC0AAAAM/ghost-of-tsushima-video-game.gif'
+                        alt='Description '
+                        width='300'
+                        height='300'></img>
+                        </div>
+                    )}
                 </div>
 
-                <div className='psConsole-items'>
+                <div className='psConsole-items'
+                     onMouseEnter={() => setIsHovered(true)}
+                     onMouseLeave={() => setIsHovered(false)}
+                >
                     <img 
                     src='https://m.media-amazon.com/images/I/81h2WhI4dtL._AC_UF1000,1000_QL80_.jpg'
                     alt="Description "
                     width='300'
                     height='300'></img>
                     <p>Elden Ring</p>
+                    <p>$39.99</p>
+
+                    {isHovered && (
+                        <div className='psGIF'>
+                            <img 
+                        src='https://i.makeagif.com/media/11-07-2021/s4K2Zt.gif'
+                        alt='Description '
+                        width='300'
+                        height='300'></img>
+                        </div>
+                    )}
                 </div>
 
-                <div className='psConsole-items'>
+                <div className='psConsole-items'
+                     onMouseEnter={() => setIsHovered(true)}
+                     onMouseLeave={() => setIsHovered(false)}
+                >
                     <img 
                     src='https://m.media-amazon.com/images/I/81J6y4tm-WL._AC_UF894,1000_QL80_.jpg'
                     alt="Description "
                     width='300'
                     height='300'></img>
                     <p>Call of Duty: Black Ops 6</p>
+                    <p>$59.99</p>
+
+
+                    {isHovered && (
+                        <div className='psGIF'>
+                            <img 
+                        src='https://i.makeagif.com/media/3-15-2023/eg9CR4.gif'
+                        alt='Description '
+                        width='300'
+                        height='300'></img>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
--- gamestore/src/Pages/Xbox/XboxBestSellers.js
+++ gamestore/src/Pages/Xbox/XboxBestSellers.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Xbox.css'
 
 const XboxBestSellers = () => {
+    const [isHovered, setIsHovered] = useState(false)
     return(
         <div>
             <div className='xboxConsole-section'>
@@ -9,40 +10,96 @@ const XboxBestSellers = () => {
             </div>
 
             <div className='xboxConsole-container'>
-                <div className='xboxConsole-items'>
+                <div className='xboxConsole-items'
+                     onMouseEnter={() => setIsHovered(true)}
+                     onMouseLeave={() => setIsHovered(false)}
+                >
                     <img 
                     src='https://m.media-amazon.com/images/I/81P8F64+ndL._AC_UF1000,1000_QL80_.jpg'
                     alt="Description "
                     width='300'
                     height='300'></img>
                     <p>NBA 2K25</p>
+                    <p>$39.99</p>
+
+                    {isHovered && (
+                        <div className='xboxGIF'>
+                            <img 
+                        src='https://assets.2k.com/1a6ngf98576c/7yZYOba3LzlVg1avmwojna/084f79ac806fbe2a38da69d1e142cdb7/NBA25_ALL_THE_CITY_REC_GIF_07_ENUS_NO_RATING_AGN_1216x684_FINAL.gif'
+                        alt='Description '
+                        width='300'
+                        height='300'></img>
+                        </div>
+                    )}
                 </div>
                 
-                <div className='xboxConsole-items'>
+                <div className='xboxConsole-items'
+                     onMouseEnter={() => setIsHovered(true)}
+                     onMouseLeave={() => setIsHovered(false)}
+                >
                     <img 
                     src='https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTdmuTIfP7YWN5virazuWh6cEcwz8ASSjs8B2LpxIVyVAwdx6_lI0Av77qOfPwSvoWvONaHP0G14crFJRwEMobnFRzIyGg7WYupeEwd22g&usqp=CAc'
                     alt="Description "
                     width='300'
                     height='300'></img>
                     <p>Assassin's Creed: Shadows</p>
+                    <p>$44.99</p>
+
+                    {isHovered && (
+                        <div className='xboxGIF'>
+                            <img 
+                        src='https://media2.giphy.com/media/JBzWx7tz59BXJ3w7Uw/giphy-downsized.gif'
+                        alt='Description '
+                        width='300'
+                        height='300'></img>
+                        </div>
+                    )}
                 </div>
 
-                <div className='xboxConsole-items'>
+                <div className='xboxConsole-items'
+                     onMouseEnter={() => setIsHovered(true)}
+                     onMouseLeave={() => setIsHovered(false)}
+                >
                     <img 
                     src='https://m.media-amazon.com/images/I/81TOOo9I-CL._AC_UF894,1000_QL80_.jpg'
                     alt="Description "
                     width='300'
                     height='300'></img>
                     <p>Street Fighter 6</p>
+                    <p>$19.99</p>
+
+                    {isHovered && (
+                        <div className='xboxGIF'>
+                            <img 
+                        src='https://miro.medium.com/v2/resize:fit:1200/1*IJ0rJqoWZZ2NfV-IniTW5Q.gif'
+                        alt='Description '
+                        width='300'
+                        height='300'></img>
+                        </div>
+                    )}
                 </div>
 
-                <div className='xboxConsole-items'>
+                <div className='xboxConsole-items'
+                     onMouseEnter={() => setIsHovered(true)}
+                     onMouseLeave={() => setIsHovered(false)}
+                >
                     <img 
                     src='https://m.media-amazon.com/images/I/81EdS+mg9sL._AC_UF1000,1000_QL80_.jpg'
                     alt="Description "
                     width='300'
                     height='300'></img>
                     <p>WWE 2k24</p>
+                    <p>$29.99</p>
+
+                    {isHovered && (
+                        <div className='xboxGIF'>
+                            <img 
+                        src='https://i.redd.it/yitcqrz8vap41.gif'
+                        alt='Description '
+                        width='300'
+                        height='300'></img>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
