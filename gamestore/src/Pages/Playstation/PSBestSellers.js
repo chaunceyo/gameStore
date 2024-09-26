85ca95af15170c10503263447e9cbeb68a1346c0
Chaunceyo <chaunceyogletree725@gmail.com>
2024-09-26 02:24:11 -0400

    first working GIF

--- gamestore/src/Pages/Playstation/PSBestSellers.js
+++ gamestore/src/Pages/Playstation/PSBestSellers.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Playstation.css'
 
 const PSBestSellers = () => {
+    const [isHovered, setIsHovered] = useState(false)
     return(
         <div>
             <div className='psConsole-section'>
@@ -10,12 +11,29 @@ const PSBestSellers = () => {
 
             <div className='psConsole-container'>
                 <div className='psConsole-items'>
-                    <img 
-                    src='https://m.media-amazon.com/images/I/81WUPcfQ9OL._AC_UF1000,1000_QL80_.jpg'
-                    alt="Description "
-                    width='300'
-                    height='300'></img>
-                    <p>Marvel's Spiderman 2</p>
+                    <div className='psSpiderman' 
+                            onMouseEnter={() => setIsHovered(true)}
+                            onMouseLeave={() => setIsHovered(false)}
+                    >
+                        <img 
+                        src='https://m.media-amazon.com/images/I/81WUPcfQ9OL._AC_UF1000,1000_QL80_.jpg'
+                        alt="Description "
+                        width='300'
+                        height='300'></img>
+                        <p>Marvel's Spiderman 2</p>
+                        <p>$39.99</p>
+
+
+                    {isHovered && (
+                        <div className='psGIF'>
+                            <img 
+                        src='https://media.tenor.com/9ATcv6bNTawAAAAM/peter-venom.gif'
+                        alt='Description '
+                        width='300'
+                        height='300'></img>
+                        </div>
+                    )}
+                    </div>
                 </div>
                 
                 <div className='psConsole-items'>
