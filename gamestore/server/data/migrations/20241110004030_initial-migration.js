8a2ae7183438948834354f3540526938b7ebb401
Chaunceyo <chaunceyogletree725@gmail.com>
2024-11-09 21:01:31 -0500

    wip

--- gamestore/server/data/migrations/20241110004030_initial-migration.js
+++ gamestore/server/data/migrations/20241110004030_initial-migration.js
@@ -4,13 +4,22 @@ exports.up = async function(knex) {
   .createTable('consoles', table => {
     table.increments('console_id')
     table.string('console_name', 200).notNullable().unique()
-    table.string('console_price', 200).notNullable()
+    table.integer('console_price', 200).notNullable()
     table.string('console_img', 200).notNullable()
     table.string('console_type', 200).notNullable()
   })
   .createTable('games', table => {
     table.increments('game_id')
     table.string('game_name', 200).notNullable().unique()
+    table.integer('game_price', 200).notNullable()
+    table.string('game_img', 200).notNullable()
+    table.string('game_gif', 200).notNullable()
+    table.string('console_type', 200)
+        .notNullable()
+        .references('console_type')
+        .inTable('consoles')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')
   })
 };
 
