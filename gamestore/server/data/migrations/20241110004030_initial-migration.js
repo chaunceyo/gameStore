ed5c487d34d36ceb0460c1d57be46f9e46fe16f3
Chaunceyo <chaunceyogletree725@gmail.com>
2024-11-09 20:23:05 -0500

    wip knex table

--- gamestore/server/data/migrations/20241110004030_initial-migration.js
+++ gamestore/server/data/migrations/20241110004030_initial-migration.js
@@ -1,8 +1,16 @@
 
 exports.up = async function(knex) {
-  
+  await knex.schema
+  .createTable('consoles', table => {
+    table.increments()
+  })
+  .createTable('games', table => {
+    table.increments()
+  })
 };
 
 exports.down = async function(knex) {
-  
+  await knex.schema
+  .dropTableIfExists('games')  
+  .dropTableIfExists('consoles')
 };
