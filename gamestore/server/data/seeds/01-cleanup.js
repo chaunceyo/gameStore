f527f9c5003bc3c53d482b2fe73e205709fe6377
Chaunceyo <chaunceyogletree725@gmail.com>
2024-12-08 22:30:19 -0500

    wip

--- gamestore/server/data/seeds/01-cleanup.js
+++ gamestore/server/data/seeds/01-cleanup.js
@@ -2,7 +2,7 @@ const { clean } = require('knex-cleaner')
 
 exports.seed = function(knex){
     return clean(knex, {
-        mode: 'truncate',
+        mode: 'truncate', //fix this file
         ignoreTables: ['knex_migrations', 'knex_migrations_lock']
     })
 }
