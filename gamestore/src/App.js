3877afbf757153d04f5c483c280246f8ac630f2c
Chaunceyo <chaunceyogletree725@gmail.com>
2024-12-12 03:04:05 -0500

    wip

--- gamestore/src/App.js
+++ gamestore/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import { Auth0ProviderWithNavigate } from './Auth0/auth0-provider-with-navigate';
 import './App.css';
+import Header from './Header/header';
 import NavBar from './NavBar/NavBar';
 import Home from './Pages/Home/Home';
 import PlayStation from './Pages/Playstation/Playstation'
@@ -14,9 +16,7 @@ function App() {
   return (
     <Router>
     <div className="App">
-      <header className="App-header">
-        <h1>gameStore</h1>
-      </header>
+     <Header/>
       <NavBar/>
       <nav className='App-nav'>
         <Routes>
--- gamestore/src/index.js
+++ gamestore/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Auth0ProviderWithNavigate } from './Auth0/auth0-provider-with-navigate';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -9,11 +10,13 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <Auth0ProviderWithNavigate>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Auth0ProviderWithNavigate>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Auth0ProviderWithNavigate>
+        <App />
+      </Auth0ProviderWithNavigate>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
