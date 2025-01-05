912e48b5b469d4efde2978c8ad8f19b4d3a92cad
Chaunceyo <chaunceyogletree725@gmail.com>
2025-01-05 04:14:37 -0500

    wip

--- gamestore/src/App.js
+++ gamestore/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Router, Route, Routes } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
 import './App.css';
-import Header from './Pages/Header/header';
+import Header from './Pages/Header/Header';
 import NavBar from './Pages/NavBar/NavBar';
 import Home from './Pages/Home/Home';
 import PlayStation from './Pages/Playstation/Playstation'
--- gamestore/src/Pages/Header/header.js
+++ gamestore/src/Pages/Header/header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import '../App.css';
-import { LogoutButton } from '../../Auth0/logoutButton';
 import { LoginButton } from '../../Auth0/loginButton';
+import { LogoutButton } from '../../Auth0/logoutButton';
 import { SignupButton } from '../../Auth0/signup-button';
+import './Header.css'
+
+
 
 const Header = () => {
     const { isAuthenticated } = useAuth0();
